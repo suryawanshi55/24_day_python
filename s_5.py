a28e95c10d77eb574803168de9e34b24b72b7271
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-04 12:14:09 +0530

    Add files via upload

---
+++ s_5.py
@@ -0,0 +1,35 @@
+# ......bytes....
+# a=b"hello"
+# print(a,type(a))     #b'hello' <class 'bytes'>
+# print(list(a))        # [104, 101, 108, 108, 111]
+# print(set(a))          #{104, 108, 101, 111}
+# print(tuple(a))       # (104, 101, 108, 108, 111) 
+# d=b'python'
+# print(d,type(d))    # b'python' <class 'bytes'>
+# print(list(d))      #[112, 121, 116, 104, 111, 110]
+# print(set(d))     #{104, 110, 111, 112, 116, 121}
+# print(tuple(d))    #(112, 121, 116, 104, 111, 110)
+# f=b"homework"
+# print(list(f))     ## {101, 104, 107, 109, 111, 114, 119} 
+# print(tuple(f))    #(104, 111, 109, 101, 119, 111, 114, 107)
+# x=b"this is the heaven"
+# print(list(x))      [116, 104, 105, 115, 32, 105, 115, 32, 116, 104, 101, 32, 104, 101, 97, 118, 101, 110]
+# s=b"neha"
+# print(list(s))     # [110, 101, 104, 97]
+# print(set(s))      # {104, 97, 101, 110}
+# print(tuple(s))    # (110, 101, 104, 97)
+# h="list"
+# print(ord('l'))    # 108
+# print(ord('i'))    # 105
+# print(ord('s'))    # 116
+# print(chr(80))       #p
+# print(chr(122))z
+# print(chr(99))c
+# print(chr(56))8
+# print(chr(34))"
+# print(chr(98))b
+# print(chr(57))9
+# print(chr(48)) 0
+
+
+
